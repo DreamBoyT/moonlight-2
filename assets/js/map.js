e6527bb1d80d1afa6095a070294b0b827467e988
DreamBoyT <115417933+DreamBoyT@users.noreply.github.com>
2024-06-19 11:21:40 +0530

    main_push

---
+++ assets/js/map.js
@@ -0,0 +1,32 @@
+   document.getElementById('tour-button-1').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-1');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-2').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-2');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968374700!6m8!1m7!1sCAoSLEFGMVFpcE9WYjVKNVVaRERwR2Jxd05jLXdibmxMS3VTUUhtcjlFaWI4WHZJ!2m2!1d12.9889158!2d79.9701828!3f22.91194893041091!4f7.888558850467788!5f0.7820865974627469" width="457" height="400" style="border:0;  border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-3').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-3');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-4').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-4');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-5').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-5');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-6').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-6');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-7').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-7');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
+   document.getElementById('tour-button-8').addEventListener('click', function() {
+     const imageContainer = document.getElementById('image-container-8');
+     imageContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!4v1715968092572!6m8!1m7!1sCAoSLEFGMVFpcE94ZEttZnNrbEZUcTZxNmhfUzluWEZGTGFLeXVMZW9VVXhxWXdi!2m2!1d12.9889158!2d79.9701828!3f106.22676578914417!4f2.7472638802313867!5f0.7820865974627469" width="457" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+   });
---
+++ assets/js/scroll.js
@@ -0,0 +1,8 @@
+// main for boys and girls smooth scrolling
+
+$(document).ready(function() {
+  AOS.init({
+    duration: 1000,
+    once: true,
+  });
+});
